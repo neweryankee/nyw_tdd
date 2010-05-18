3f00a78b9c47eeda24490279ca9e0935c4c9da2e
Greg Sterndale <gsterndale@gmail.com>
2010-05-17 21:41:59 -0400

    Schedule .new with date string

--- lib/schedule.rb
+++ lib/schedule.rb
@@ -2,6 +2,11 @@ class Schedule
 
   attr_accessor :date
 
+  def initialize(date_string=nil)
+    self.date = Time.parse(date_string) if date_string
+    super()
+  end
+
   def plan
     return 'No plan' unless self.date.is_a? Time
     if (1..5).include? self.date.wday
