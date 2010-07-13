fd1f0fd8534b9633f4383a48a98494ff2ed586eb
Greg Sterndale <gsterndale@gmail.com>
2010-07-13 01:04:42 -0400

    Look in project directory for binaries and files to include

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,2 +1,14 @@
 require 'aruba'
 
+module ArubaOverrides
+  def detect_ruby_script(cmd)
+    if cmd =~ /^impromptu /
+      "ruby -I../../ -S ../../bin/#{cmd}"
+    else
+      super(cmd)
+    end
+  end
+end
+
+World(ArubaOverrides)
+
